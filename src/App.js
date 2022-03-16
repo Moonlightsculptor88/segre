039a92b339d1295035a57027524a1bc50f337b81
Pritam Reddy <48624160+Moonlightsculptor88@users.noreply.github.com>
2022-03-16 18:43:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -119,7 +119,7 @@ function App() {
               setHeading('CUSTOMER LIST')
             }}>List</button></div>
             
-            </div>
+            
           </div>
     </div>
   );
