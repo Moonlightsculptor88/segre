1ae0278acb3e5291fa346c0d43c8019648d31bfe
Pritam Reddy <48624160+Moonlightsculptor88@users.noreply.github.com>
2022-03-16 17:49:27 +0530

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
+
+const styles = {
+  opacity : 0.9,
+  pointerEvent: "all"
+}
 
 function List(props) {
   
+  const submit = (id, token) => {
 
+    confirmAlert({
+      title: 'Confirm to submit',
+      message: 'Are you sure to do this.',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => props.delete(id, token)
+        },
+        {
+          label: 'No',
+        }
+      ]
+    });
+  }
+  
   return (
     <div>
+    
+
     <div className='table'>
     { props.users && props.users.map((cust)=>{
       
@@ -14,8 +39,10 @@ function List(props) {
           <div className='row' key={cust.token}>
             <div className='number'>{cust.token}</div>
             <div className='name'>{cust.userName}</div>
+            
             <div className='del'><button onClick={()=>{
-              props.delete(cust._id, cust.token)
+              submit(cust._id, cust.token)
+              
             }}>X</button></div>
           </div>
       
